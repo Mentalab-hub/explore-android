f98a2e3b9a8dbdd334110b1c400abea1a945f155
salman <salman2135@gmail.com>
2021-10-13 17:50:15 +0200

    added jna support

--- mentalabMobileApi/src/main/java/com/mentalab/LslPacketSubscriber.java
+++ mentalabMobileApi/src/main/java/com/mentalab/LslPacketSubscriber.java
@@ -2,13 +2,20 @@ package com.mentalab;
 
 import android.util.Log;
 
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
+import com.sun.jna.Pointer;
+
 public class LslPacketSubscriber {
 
   private static final String TAG = "EXPLORE_LSL_DEV";
 
   public LslPacketSubscriber() {
+
     Log.d(TAG, "Subscribing!!");
     PubSubManager.getInstance().subscribe("ExG", this::packetCallback);
+    System.loadLibrary("lsl");
   }
 
   public void packetCallback(Packet packet) {
