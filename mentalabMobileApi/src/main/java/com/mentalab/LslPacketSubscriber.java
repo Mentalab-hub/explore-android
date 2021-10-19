dcdfc29edbc83d48a59844ea2c36d453a919282b
salman <salman2135@gmail.com>
2021-10-19 17:14:46 +0200

    refactor LSL subscriber

--- mentalabMobileApi/src/main/java/com/mentalab/LslPacketSubscriber.java
+++ mentalabMobileApi/src/main/java/com/mentalab/LslPacketSubscriber.java
@@ -1,16 +1,11 @@
 package com.mentalab;
 
 import android.util.Log;
-
 import com.mentalab.LslLoader.StreamInfo;
-import com.sun.jna.Library;
-import com.sun.jna.Native;
-import com.sun.jna.Platform;
-import com.sun.jna.Pointer;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class LslPacketSubscriber{
+public class LslPacketSubscriber {
 
   private static final String TAG = "EXPLORE_LSL_DEV";
   static LslLoader.StreamOutlet lslStreamOutletExg;
@@ -19,57 +14,50 @@ public class LslPacketSubscriber{
   private LslLoader.StreamInfo lslStreamInfoOrn;
   static LslLoader lslLoader = new LslLoader();
 
-
   public LslPacketSubscriber() throws IOException {
 
-    lslStreamInfoExg = new StreamInfo("Explore_ExG", "ExG", 8, 250, LslLoader.ChannelFormat.float32, "ExG");
+    lslStreamInfoExg =
+        new StreamInfo("Explore_ExG", "ExG", 8, 250, LslLoader.ChannelFormat.float32, "ExG");
     if (lslStreamInfoExg == null) {
       throw new IOException("Stream Info is Null!!");
     }
     lslStreamOutletExg = new LslLoader.StreamOutlet(lslStreamInfoExg);
 
-    lslStreamInfoOrn = new StreamInfo("Explore_Orn", "Orn", 9, 20, LslLoader.ChannelFormat.float32, "Orn");
+    lslStreamInfoOrn =
+        new StreamInfo("Explore_Orn", "Orn", 9, 20, LslLoader.ChannelFormat.float32, "Orn");
     if (lslStreamInfoOrn == null) {
       throw new IOException("Stream Info is Null!!");
     }
     lslStreamOutletOrn = new LslLoader.StreamOutlet(lslStreamInfoOrn);
 
     Log.d(TAG, "Subscribing!!");
-    PubSubManager.getInstance().subscribe("ExG", this::packetCallback);
-    PubSubManager.getInstance().subscribe("Orn", this::packetCallback);
-
-
+    PubSubManager.getInstance().subscribe("ExG", this::packetCallbackExG);
+    PubSubManager.getInstance().subscribe("Orn", this::packetCallbackOrn);
   }
 
-
-  public void packetCallback(Packet packet) {
-    Log.d(TAG, "Getting data in LSL callback!!");
-    if (packet instanceof DataPacket){
-      lslStreamOutletExg.push_sample(convertArraylistToFloatArray(packet));
-    }
-    else if (packet instanceof Orientation){
-
-      lslStreamOutletOrn.push_sample(convertArraylistToFloatArray(packet));
-    }
-
+  public void packetCallbackExG(Packet packet) {
+    lslStreamOutletExg.push_sample(convertArraylistToFloatArray(packet));
   }
 
-  float[] convertArraylistToFloatArray(Packet packet){
-    ArrayList<Float> packetVoltageValues= new ArrayList<Float>();
-    if (packet instanceof DataPacket){
-    packetVoltageValues = ((DataPacket)packet).getVoltageValues();
-    float[] floatArray = new float[packetVoltageValues.size()];
-    Object[] array = packetVoltageValues.toArray();
-    for (int index = 0; index < packetVoltageValues.size(); index ++){
-      floatArray[index] = ((Float) packetVoltageValues.get(index)).floatValue();
-    }
-    return floatArray;
+  public void packetCallbackOrn(Packet packet) {
+    lslStreamOutletOrn.push_sample(convertArraylistToFloatArray(packet));
   }
-    else if(packet instanceof Orientation){
-      packetVoltageValues = ((Orientation)packet).listValues;
+
+  float[] convertArraylistToFloatArray(Packet packet) {
+    ArrayList<Float> packetVoltageValues = new ArrayList<Float>();
+    if (packet instanceof DataPacket) {
+      packetVoltageValues = ((DataPacket) packet).getVoltageValues();
       float[] floatArray = new float[packetVoltageValues.size()];
       Object[] array = packetVoltageValues.toArray();
-      for (int index = 0; index < packetVoltageValues.size(); index ++){
+      for (int index = 0; index < packetVoltageValues.size(); index++) {
+        floatArray[index] = ((Float) packetVoltageValues.get(index)).floatValue();
+      }
+      return floatArray;
+    } else if (packet instanceof Orientation) {
+      packetVoltageValues = ((Orientation) packet).listValues;
+      float[] floatArray = new float[packetVoltageValues.size()];
+      Object[] array = packetVoltageValues.toArray();
+      for (int index = 0; index < packetVoltageValues.size(); index++) {
         floatArray[index] = ((Float) packetVoltageValues.get(index)).floatValue();
       }
       return floatArray;
