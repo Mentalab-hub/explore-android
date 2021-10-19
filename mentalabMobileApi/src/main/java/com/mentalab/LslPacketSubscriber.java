5571c6157c949f0140538c3195a5ed7664540cdb
salman <salman2135@gmail.com>
2021-10-19 17:49:08 +0200

    add in same call back

--- mentalabMobileApi/src/main/java/com/mentalab/LslPacketSubscriber.java
+++ mentalabMobileApi/src/main/java/com/mentalab/LslPacketSubscriber.java
@@ -38,11 +38,11 @@ public class LslPacketSubscriber {
   public void packetCallbackExG(Packet packet) {
     Log.d("TAG", "packetCallbackExG");
     lslStreamOutletExg.push_sample(convertArraylistToFloatArray(packet));
+    lslStreamOutletOrn.push_sample(convertArraylistToFloatArray(packet));
   }
 
   public void packetCallbackOrn(Packet packet) {
     Log.d("TAG", "packetCallbackOrn");
-    //lslStreamOutletOrn.push_sample(convertArraylistToFloatArray(packet));
   }
 
   float[] convertArraylistToFloatArray(Packet packet) {
