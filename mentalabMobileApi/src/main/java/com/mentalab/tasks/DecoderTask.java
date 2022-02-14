d134f5f2ccbd9b8affba8392f1ef402b69f3ad03
salman <salman2135@gmail.com>
2022-02-14 12:33:27 +0100

    change to support data acquisition with basic queue structure

--- mentalabMobileApi/src/main/java/com/mentalab/tasks/DecoderTask.java
+++ mentalabMobileApi/src/main/java/com/mentalab/tasks/DecoderTask.java
@@ -2,13 +2,11 @@ package com.mentalab.tasks;
 
 import android.util.Log;
 import com.mentalab.PubSubManager;
-import com.mentalab.utils.Utils.*;
 import com.mentalab.exception.InvalidDataException;
 import com.mentalab.packets.Packet;
 import com.mentalab.packets.PacketId;
 import com.mentalab.packets.PublishablePacket;
 import com.mentalab.packets.QueueablePacket;
-import com.mentalab.utils.Utils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
@@ -26,71 +24,69 @@ public class DecoderTask implements Callable<Void> {
   private final InputStream inputStream;
   private static final String TAG = "Explore";
 
-    public DecoderTask(InputStream inputStream) {
-      this.inputStream = inputStream;
-      initializeMapInstance();
-    }
+  public DecoderTask(InputStream inputStream) {
+    this.inputStream = inputStream;
+    initializeMapInstance();
+  }
 
-    public Void call() {
+  public Void call() {
 
-      int pId = 0;
-      while (!Thread.currentThread().isInterrupted()) {
-        try {
-          byte[] buffer = new byte[1024];
-          // reading PID
-          inputStream.read(buffer, 0, 1);
-          pId = ByteBuffer.wrap(buffer).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
-          Log.d(TAG, "pid .." + pId);
-          buffer = new byte[1024];
+    int pId = 0;
+    while (!Thread.currentThread().isInterrupted()) {
+      try {
+        byte[] buffer = new byte[1024];
+        // reading PID
+        inputStream.read(buffer, 0, 1);
+        pId = ByteBuffer.wrap(buffer).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
+        Log.d(TAG, "pid .." + pId);
+        buffer = new byte[1024];
 
-          // reading count
-          inputStream.read(buffer, 0, 1);
-          int count = ByteBuffer.wrap(buffer).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
-          buffer = new byte[1024];
+        // reading count
+        inputStream.read(buffer, 0, 1);
+        int count = ByteBuffer.wrap(buffer).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
+        buffer = new byte[1024];
 
-          // reading payload
-          inputStream.read(buffer, 0, 2);
-          int payload = ByteBuffer.wrap(buffer).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
-          buffer = new byte[1024];
+        // reading payload
+        inputStream.read(buffer, 0, 2);
+        int payload = ByteBuffer.wrap(buffer).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
+        buffer = new byte[1024];
 
-          // reading timestamp
-          inputStream.read(buffer, 0, 4);
-          double timeStamp =
-              ByteBuffer.wrap(buffer).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
-          timeStamp = timeStamp / 10_000; // convert to seconds
+        // reading timestamp
+        inputStream.read(buffer, 0, 4);
+        double timeStamp = ByteBuffer.wrap(buffer).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
+        timeStamp = timeStamp / 10_000; // convert to seconds
 
-          Log.d(TAG, "pid .." + pId + " payload is : " + payload);
+        Log.d(TAG, "pid .." + pId + " payload is : " + payload);
 
-          // reading payload data
-          buffer = new byte[payload - 4];
-          int read = inputStream.read(buffer, 0, buffer.length);
-          Log.d(TAG, "reading count is ...." + read);
-          // parsing payload data
+        // reading payload data
+        buffer = new byte[payload - 4];
+        int read = inputStream.read(buffer, 0, buffer.length);
+        Log.d(TAG, "reading count is ...." + read);
+        // parsing payload data
 
-          Packet packet = parsePayloadData(pId, timeStamp, Arrays
-              .copyOfRange(buffer, 0, buffer.length - 4));
-          if (packet instanceof QueueablePacket){
-            pushDataInQueue(packet);
-          }
-          if (packet instanceof PublishablePacket){
-            PubSubManager
-                .getInstance().publish(((PublishablePacket) packet).getTopic().toString(), packet);
-          }
-
-        } catch (IOException | InvalidDataException exception) {
-          Thread.currentThread().interrupt();
+        Packet packet =
+            parsePayloadData(pId, timeStamp, Arrays.copyOfRange(buffer, 0, buffer.length - 4));
+        if (packet instanceof QueueablePacket) {
+          pushDataInQueue(packet);
         }
+        if (packet instanceof PublishablePacket) {
+          PubSubManager.getInstance()
+              .publish(((PublishablePacket) packet).getTopic().toString(), packet);
+        }
+
+      } catch (IOException | InvalidDataException exception) {
+        Thread.currentThread().interrupt();
       }
-      return null;
     }
+    return null;
+  }
 
-    void initializeMapInstance() {
+  void initializeMapInstance() {
 
-      if (decodedDataMap == null) {
-        decodedDataMap = new HashMap<>();
-      }
+    if (decodedDataMap == null) {
+      decodedDataMap = new HashMap<>();
     }
-
+  }
 
   private static Packet parsePayloadData(int pId, double timeStamp, byte[] byteBuffer)
       throws InvalidDataException {
@@ -108,9 +104,10 @@ public class DecoderTask implements Callable<Void> {
     }
     return null;
   }
+
   private static void pushDataInQueue(Packet packet) {
 
-    if (packet instanceof QueueablePacket){
+    if (packet instanceof QueueablePacket) {
       int channelCount = packet.getDataCount();
       ArrayList<Float> convertedSamples = packet.getData();
       List<String> attributes = packet.attributes;
@@ -129,7 +126,7 @@ public class DecoderTask implements Callable<Void> {
     }
   }
 
-  public static Map<String, Queue<Float>>getDataMap(){
-  return decodedDataMap;
+  public static Map<String, Queue<Float>> getDataMap() {
+    return decodedDataMap;
   }
 }
