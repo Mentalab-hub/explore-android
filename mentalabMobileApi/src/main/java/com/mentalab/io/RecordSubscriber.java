8c92eef8a1886e407c736badb2e6d00d99d8fdf3
Nujanauss <appacclap@hotmail.co.uk>
2022-06-15 15:32:55 +0200

    formatting

--- mentalabMobileApi/src/main/java/com/mentalab/io/RecordSubscriber.java
+++ mentalabMobileApi/src/main/java/com/mentalab/io/RecordSubscriber.java
@@ -10,51 +10,50 @@ import java.io.IOException;
 
 public class RecordSubscriber extends Subscriber {
 
-    protected final BufferedWriter wr;
+  protected final BufferedWriter wr;
 
+  public RecordSubscriber(Topic t, BufferedWriter w) {
+    super(t);
+    this.wr = w;
+  }
 
-    public RecordSubscriber(Topic t, BufferedWriter w) {
-        super(t);
-        this.wr = w;
+  @Override
+  public void accept(Packet packet) {
+    try {
+      writePacketToCSV(wr, packet);
+    } catch (IOException e) {
+      Log.e(Utils.TAG, e.getMessage());
     }
+  }
 
-    @Override
-    public void accept(Packet packet) {
-        try {
-            writePacketToCSV(wr, packet);
-        } catch (IOException e) {
-            Log.e(Utils.TAG, e.getMessage());
-        }
-    }
-
-    protected void writePacketToCSV(BufferedWriter writer, Packet packet) throws IOException {
-        double currentTimestamp = packet.getTimeStamp();
+  protected void writePacketToCSV(BufferedWriter writer, Packet packet) throws IOException {
+    double currentTimestamp = packet.getTimeStamp();
+    initNewLine(writer, currentTimestamp);
+    for (int i = 0; i < packet.getData().size(); i++) {
+      writeToLine(writer, packet, i);
+      if (requireNewLine(packet, i)) {
         initNewLine(writer, currentTimestamp);
-        for (int i = 0; i < packet.getData().size(); i++) {
-            writeToLine(writer, packet, i);
-            if (requireNewLine(packet, i)) {
-                initNewLine(writer, currentTimestamp);
-            }
-        }
+      }
     }
+  }
 
-    protected void writeToLine(BufferedWriter writer, Packet packet, int i) throws IOException {
-        writer.write(",");
-        writer.write(packet.getData().get(i).toString());
-    }
+  protected void writeToLine(BufferedWriter writer, Packet packet, int i) throws IOException {
+    writer.write(",");
+    writer.write(packet.getData().get(i).toString());
+  }
 
-    protected static boolean requireNewLine(Packet p, int i) {
-        if (i == p.getData().size() - 1) {
-            return false;
-        }
-        final int channelCount = p.getDataCount();
-        final int channelNo = i % channelCount + 1; // 1, 2, 3, 4,...
-        return channelNo == channelCount; // break line after 2, 4 or 8 entries
+  protected static boolean requireNewLine(Packet p, int i) {
+    if (i == p.getData().size() - 1) {
+      return false;
     }
+    final int channelCount = p.getDataCount();
+    final int channelNo = i % channelCount + 1; // 1, 2, 3, 4,...
+    return channelNo == channelCount; // break line after 2, 4 or 8 entries
+  }
 
-    protected static void initNewLine(BufferedWriter writer, double ts) throws IOException {
-        writer.newLine();
-        writer.write(Utils.round(ts));
-        writer.flush();
-    }
+  protected static void initNewLine(BufferedWriter writer, double ts) throws IOException {
+    writer.newLine();
+    writer.write(Utils.round(ts));
+    writer.flush();
+  }
 }
