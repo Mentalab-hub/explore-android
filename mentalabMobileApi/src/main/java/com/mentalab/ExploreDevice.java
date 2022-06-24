f657e9a9c0399ec760e46f6ac44975607959c7ec
Nujanauss <appacclap@hotmail.co.uk>
2022-06-24 15:25:04 +0200

    formatting

--- mentalabMobileApi/src/main/java/com/mentalab/ExploreDevice.java
+++ mentalabMobileApi/src/main/java/com/mentalab/ExploreDevice.java
@@ -50,7 +50,8 @@ public class ExploreDevice {
    * @param switches List of channels to set on (true) or off (false) channel0 ... channel7
    * @throws InvalidCommandException If the provided Switches are not all type Channel.
    */
-  public Future<Boolean> setChannels(Set<ConfigSwitch> switches) throws InvalidCommandException, IOException, NoBluetoothException {
+  public Future<Boolean> setChannels(Set<ConfigSwitch> switches)
+      throws InvalidCommandException, IOException, NoBluetoothException {
     Utils.checkSwitchTypes(switches, ConfigProtocol.Type.Channel);
     final Command c = generateChannelCommand(switches);
     return submitCommand(c);
@@ -83,7 +84,8 @@ public class ExploreDevice {
    * @param channel Switch The channel you would like to turn on (true) or off (false).
    * @throws InvalidCommandException If the provided Switch is not of type Channel.
    */
-  public Future<Boolean> setChannel(ConfigSwitch channel) throws InvalidCommandException, IOException, NoBluetoothException {
+  public Future<Boolean> setChannel(ConfigSwitch channel)
+      throws InvalidCommandException, IOException, NoBluetoothException {
     final Set<ConfigSwitch> channelToList = new HashSet<>();
     channelToList.add(channel);
     return setChannels(channelToList);
@@ -97,7 +99,8 @@ public class ExploreDevice {
    *
    * @param mSwitch The module to be turned on or off ORN, ENVIRONMENT, EXG
    */
-  public Future<Boolean> setModule(ConfigSwitch mSwitch) throws InvalidCommandException, IOException, NoBluetoothException {
+  public Future<Boolean> setModule(ConfigSwitch mSwitch)
+      throws InvalidCommandException, IOException, NoBluetoothException {
     Utils.checkSwitchType(mSwitch, ConfigProtocol.Type.Module);
     final Command c = generateModuleCommand(mSwitch);
     return submitCommand(c);
@@ -157,9 +160,11 @@ public class ExploreDevice {
    * @return Future True if the command was successfully received. Otherwise false
    * @throws InvalidCommandException If the command cannot be encoded.
    */
-  private Future<Boolean> submitCommand(Command c) throws InvalidCommandException, IOException, NoBluetoothException {
+  private Future<Boolean> submitCommand(Command c)
+      throws InvalidCommandException, IOException, NoBluetoothException {
     final byte[] encodedBytes = encodeCommand(c);
-    return ExploreExecutor.submitTask(new DeviceConfigurationTask(BluetoothManager.getOutputStream(), encodedBytes));
+    return ExploreExecutor.submitTask(
+        new DeviceConfigurationTask(BluetoothManager.getOutputStream(), encodedBytes));
   }
 
   private static byte[] encodeCommand(Command c) throws InvalidCommandException {
