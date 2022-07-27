1f31ed818608e979fab88f260e0821ffa6739acc
Nujanauss <appacclap@hotmail.co.uk>
2022-07-27 11:58:53 +0200

    Removed unnecessary concurrency

--- mentalabMobileApi/src/main/java/com/mentalab/service/io/ContentServer.java
+++ mentalabMobileApi/src/main/java/com/mentalab/service/io/ContentServer.java
@@ -3,14 +3,14 @@ package com.mentalab.service.io;
 import com.mentalab.packets.Packet;
 import com.mentalab.utils.constants.Topic;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class ContentServer {
 
-  // it is a good idea to provide a size estimate as an optional initialCapacity
-  private final Map<Topic, Set<Subscriber>> topicSubscribers = new ConcurrentHashMap<>(5);
+  private final Map<Topic, Set<Subscriber>> topicSubscribers = new HashMap<>();
 
   public static ContentServer getInstance() {
     return InstanceHolder.INSTANCE;
@@ -28,9 +28,7 @@ public class ContentServer {
   }
 
   public void registerSubscriber(Subscriber sub) {
-    this.topicSubscribers
-            .computeIfAbsent(sub.getTopic(), k -> ConcurrentHashMap.newKeySet())
-            .add(sub);
+    this.topicSubscribers.computeIfAbsent(sub.getTopic(), k -> new HashSet<>()).add(sub);
   }
 
   public void deRegisterSubscriber(Subscriber sub) {
@@ -43,6 +41,6 @@ public class ContentServer {
   private ContentServer() {}
 
   private static class InstanceHolder { // Initialization-on-demand synchronization
-    private static final ContentServer INSTANCE = new ContentServer();
+    static final ContentServer INSTANCE = new ContentServer();
   }
 }
